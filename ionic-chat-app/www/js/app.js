6b0c5c56acae8f89f1148ede641115b628707026
Sagar Thapa <Sagar Thapa>
2017-07-16 00:39:25 +0100

    add main app set up

--- ionic-chat-app/www/js/app.js
+++ ionic-chat-app/www/js/app.js
@@ -5,81 +5,129 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('IoniChatApp', ['ionic', 'chatapp.controllers', 'chatapp.services', 'chatapp.directives', 'ngCordova', 'ngCordovaOauth', 'firebase' ])
+angular.module('IoniChatApp', ['ionic', 'chatapp.controllers', 'chatapp.services', 'chatapp.directives', 'ngCordova', 'ngCordovaOauth', 'firebase'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+.run(function ($ionicPlatform, $rootScope, $state) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+
+        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+            if (error === 'AUTH_REQUIRED') {
+                $state.go('main');
+            }
+        })
+    });
 })
+.constant('FBURL', 'https://ionic-chat-app-2100b.firebaseio.com/')
+.constant('GOOGLEKEY', '781336069790-tk3okjf01d6vji54mu1q2uaqnnokqga4.apps.googleusercontent.com')
+.constant('GOOGLEAUTHSCOPE', ['email'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    $ionicConfigProvider.backButton.previousTitleText(false);
+    $ionicConfigProvider.views.transition('platform');
+    $ionicConfigProvider.navBar.alignTitle('center');
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+       .state('main', {
+           url: '/',
+           templateUrl: 'templates/main.html',
+           controller: 'MainCtrl',
+           cache: false,
+           resolve: {
+               'currentAuth': ['FBFactory', 'Loader', function (FBFactory, Loader) {
+                   Loader.show('Checking Auth..');
+                   return FBFactory.auth().$waitForAuth();
+               }]
+           }
+       })
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+          url: '/tab',
+          abstract: true,
+          cache: false,
+          templateUrl: 'templates/tabs.html'
+      })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+    .state('tab.dash', {
+        url: '/dash',
+        cache: false,
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }, resolve: {
+                'currentAuth': ['FBFactory', function (FBFactory) {
+                    return FBFactory.auth().$requireAuth();
+                }]
+            }
         }
-      }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+
+    .state('tab.chats', {
+        url: '/chats',
+        cache: false,
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+            }
+        },resolve: {
+            'currentAuth': ['FBFactory', function (FBFactory) {
+                return FBFactory.auth().$requireAuth();
+            }]
         }
-      }
     })
+      .state('tab.chat-detail', {
+          url: '/chats/:chatId',
+          views: {
+              'tab-chats': {
+                  templateUrl: 'templates/chat-detail.html',
+                  controller: 'ChatDetailCtrl'
+              }
+          },
+              resolve: {
+                  'currentAuth': ['FBFactory', function (FBFactory) {
+                      return FBFactory.auth().$requireAuth();
+                  }]
+              }
+          
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .state('tab.account', {
+        url: '/account',
+        cache: false,
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl',
+                resolve: {
+                    'currentAuth': ['FBFactory', function (FBFactory) {
+                        return FBFactory.auth().$requireAuth();
+                    }]
+                }
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/');
 
 });
